fb63082e3477ddf7ecf6b8fa5b2f226c82899b37
Himanshu <himanshu.nxtqube@gmail.com>
2025-12-17 19:39:40 +0530

    fixed box_counter

--- src/analysis/box_counter.py
+++ src/analysis/box_counter.py
@@ -67,66 +67,67 @@ class BoxCounter():
                 fartest_boxes = max(len(fartest_box_list), 0)
                 return front_boxes + back_boxes + fartest_boxes
         elif stacking_type == "interlock":
-            if not boxes_per_layer:
-                return 0
-            top_sorted = sorted(box_list, key=lambda b: (b[1]+b[3])/2)
+            if pallet_status == "partial":
+                if not boxes_per_layer:
+                    return 0
+                top_sorted = sorted(box_list, key=lambda b: (b[1]+b[3])/2)
 
-            i = -1
-            current_count = stack_count
-            front_boxes_list = []
-            while stack_count == current_count:
-                i+=1
-                second_top_box = top_sorted[i] if i < len(top_sorted) else None
-                if second_top_box == None:
-                    break
-                rx = second_top_box[2] - 150
-                lx = second_top_box[0] + 150
-                cy = (second_top_box[1]+second_top_box[3])/2
+                i = -1
+                current_count = stack_count
+                front_boxes_list = []
+                while stack_count == current_count:
+                    i+=1
+                    second_top_box = top_sorted[i] if i < len(top_sorted) else None
+                    if second_top_box == None:
+                        break
+                    rx = second_top_box[2] - 150
+                    lx = second_top_box[0] + 150
+                    cy = (second_top_box[1]+second_top_box[3])/2
 
-                count11 = sum(1 for b in box_list if b[0] <= lx <= b[2] and b[1] >= cy)
-                count12 = sum(1 for b in box_list if b[0] <= rx <= b[2] and b[1] >= cy)
-                current_count = max(count11, count12)
-                front_boxes_list.append(second_top_box)
-            # return i + boxes_per_layer//2
-            front_horizontal_boxes = []
-            front_vertical_boxes = []
+                    count11 = sum(1 for b in box_list if b[0] <= lx <= b[2] and b[1] >= cy)
+                    count12 = sum(1 for b in box_list if b[0] <= rx <= b[2] and b[1] >= cy)
+                    current_count = max(count11, count12)
+                    front_boxes_list.append(second_top_box)
+                # return i + boxes_per_layer//2
+                front_horizontal_boxes = []
+                front_vertical_boxes = []
 
-            back_horizontal_boxes = []
-            back_vertical_boxes = []
+                back_horizontal_boxes = []
+                back_vertical_boxes = []
 
-            fartest_horizontal_boxes = []
-            fartest_vertical_boxes = []
+                fartest_horizontal_boxes = []
+                fartest_vertical_boxes = []
 
-            total_extra_boxes = 0
+                total_extra_boxes = 0
 
-            for box in front_boxes_list:
-                if abs((box[2] - box[0]) - avg_box_length) < abs((box[2] - box[0]) - avg_box_width):
-                    front_horizontal_boxes.append(box)
-                else:
-                    front_vertical_boxes.append(box)
-            
-            for box in back_box_list:
-                # normal_box_height / back_box_height -> relative height factor
-                # relative height factor * back_box_width -> width of box as if the box is at front 
-                if abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_length) < abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_width):
-                    back_horizontal_boxes.append(box)
-                else:
-                    back_vertical_boxes.append(box)
-            
-            for box in fartest_box_list:
-                if abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_length) < abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_width):
-                    fartest_horizontal_boxes.append(box)
-                else:
-                    fartest_vertical_boxes.append(box)
+                for box in front_boxes_list:
+                    if abs((box[2] - box[0]) - avg_box_length) < abs((box[2] - box[0]) - avg_box_width):
+                        front_horizontal_boxes.append(box)
+                    else:
+                        front_vertical_boxes.append(box)
+                
+                for box in back_box_list:
+                    # normal_box_height / back_box_height -> relative height factor
+                    # relative height factor * back_box_width -> width of box as if the box is at front 
+                    if abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_length) < abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_width):
+                        back_horizontal_boxes.append(box)
+                    else:
+                        back_vertical_boxes.append(box)
+                
+                for box in fartest_box_list:
+                    if abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_length) < abs((avg_box_height / (box[3] - box[1]) * (box[2] - box[0])) - avg_box_width):
+                        fartest_horizontal_boxes.append(box)
+                    else:
+                        fartest_vertical_boxes.append(box)
 
-            total_extra_boxes += len(front_horizontal_boxes) * self.interlock_strcture["horizontal"]
-            total_extra_boxes += len(front_vertical_boxes) * self.interlock_strcture["vertical"]
-            total_extra_boxes += len(back_horizontal_boxes) * max(self.interlock_strcture["horizontal"] - 1, 0)
-            total_extra_boxes += len(back_vertical_boxes) * max(self.interlock_strcture["vertical"] - 1, 0)
-            total_extra_boxes += len(fartest_horizontal_boxes) * max(self.interlock_strcture["horizontal"] - 2, 0)
-            total_extra_boxes += len(fartest_vertical_boxes) * max(self.interlock_strcture["vertical"] - 2, 0)
+                total_extra_boxes += len(front_horizontal_boxes) * self.interlock_strcture["horizontal"]
+                total_extra_boxes += len(front_vertical_boxes) * self.interlock_strcture["vertical"]
+                total_extra_boxes += len(back_horizontal_boxes) * max(self.interlock_strcture["horizontal"] - 1, 0)
+                total_extra_boxes += len(back_vertical_boxes) * max(self.interlock_strcture["vertical"] - 1, 0)
+                total_extra_boxes += len(fartest_horizontal_boxes) * max(self.interlock_strcture["horizontal"] - 2, 0)
+                total_extra_boxes += len(fartest_vertical_boxes) * max(self.interlock_strcture["vertical"] - 2, 0)
 
-            return total_extra_boxes
+                return total_extra_boxes
             
         return 0
     
