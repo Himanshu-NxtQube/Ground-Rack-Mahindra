44022af0cfa6962774a3748b79e01493514d41e4
Himanshu <himanshu.nxtqube@gmail.com>
2026-01-13 11:50:55 +0530

    chore(box_detector): updated .gitignore and updated ground box model path

--- src/inference/box_detector.py
+++ src/inference/box_detector.py
@@ -4,7 +4,7 @@ import cv2
 
 class BoxDetector:
     def __init__(self):
-        self.model = YOLO("models/Mahindra_g_box_17_back.pt", verbose=False)
+        self.model = YOLO("models/New Ground Box model.pt", verbose=False)
 
     def filter_front_boxes(self, left_boxes, right_boxes, left_pallet, right_pallet, depth_map):
         if left_pallet is not None:
