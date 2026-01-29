de0a7bd1bdb823d4df375d4a4bdd46bde7b2dec6
Himanshu <himanshu.nxtqube@gmail.com>
2026-01-29 12:03:53 +0530

    feat: Add system path configuration and module imports for inference and utility functions to `get_box_depth.py`.

--- debug/vision/get_box_depth.py
+++ debug/vision/get_box_depth.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from src.inference import boundary_detection, depth_estimation, pallet_detector, box_detector
 from src.utils import visualizer
 
