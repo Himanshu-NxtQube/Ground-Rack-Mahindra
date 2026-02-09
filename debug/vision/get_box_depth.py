4839073fa34fb3c6dcc824337812f090e09b4a1e
Himanshu <himanshu.nxtqube@gmail.com>
2026-02-09 14:29:19 +0530

    Updated the scripts as per the main project

--- debug/vision/get_box_depth.py
+++ debug/vision/get_box_depth.py
@@ -2,23 +2,39 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from src.inference import boundary_detection, depth_estimation, pallet_detector, box_detector
+from src.inference import boundary_detection, depth_estimation, pallet_detector, box_detector, ocr_parser
 from src.utils import visualizer
 
 boundary_detector = boundary_detection.BoundaryDetector()
 depth_estimator = depth_estimation.DepthEstimator("depth_anything_v2")
 pallet_detector = pallet_detector.PalletDetector()
 box_detector = box_detector.BoxDetector()
+ocr_parser = ocr_parser.OCRParser()
 visualizer = visualizer.Visualizer()
 
 images_dir = 'images/'
 
-def process_single_image(image_path):
+def initialize(image_path):
+    # These are independent tasks
     boundaries = boundary_detector.get_boundaries(image_path)
+    annotations = ocr_parser.get_annotations(image_path)
     depth_map = depth_estimator.get_depth_map(image_path)
+    pallets = pallet_detector.detect(image_path)
+    boxes = box_detector.detect(image_path)
+    return boundaries, annotations, depth_map, pallets, boxes
+
+def process_single_image(image_path):
+    # boundaries = boundary_detector.get_boundaries(image_path)
+    # depth_map = depth_estimator.get_depth_map(image_path)
     
-    left_pallet, right_pallet = pallet_detector.detect(image_path, boundaries)
-    left_boxes, right_boxes = box_detector.detect(image_path, boundaries, left_pallet, right_pallet)
+    # left_pallet, right_pallet = pallet_detector.detect(image_path, boundaries)
+    # left_boxes, right_boxes = box_detector.detect(image_path, boundaries, left_pallet, right_pallet)
+
+    image_shape = cv2.imread(image_path).shape
+
+    boundaries, annotations, depth_map, pallets, boxes = initialize(image_path)
+    left_pallet, right_pallet = pallet_detector.filter_and_split_pallets(image_path, boundaries, image_shape[1])
+    left_boxes, right_boxes = box_detector.map_boxes(boxes, left_pallet, right_pallet)
     
     visualizer.visualize_box_dimensions(image_path, "left", [left_boxes], left_pallet, depth_map)
     visualizer.visualize_box_dimensions(image_path, "right", [right_boxes], right_pallet, depth_map)
--- debug/vision/visualize_box_dimensions.py
+++ debug/vision/visualize_box_dimensions.py
@@ -3,49 +3,54 @@ import sys
 import cv2
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from src.inference.boundary_detection import BoundaryDetector
 from src.inference.box_detector import BoxDetector
 from src.inference.pallet_detector import PalletDetector
+from src.inference.boundary_detection import BoundaryDetector
 
-boundary_detector = BoundaryDetector()
 box_detector = BoxDetector()
 pallet_detector = PalletDetector()
+boundary_detector = BoundaryDetector()
 
 images_dir = 'images/'
+
+
+def initialize(image_path):
+    # These are independent tasks
+    boundaries = boundary_detector.get_boundaries(image_path)
+    pallets = pallet_detector.detect(image_path)
+    boxes = box_detector.detect(image_path)
+    return boundaries, pallets, boxes
 
 def visualize_box_dimensions(image_path, boxes):
     image = cv2.imread(image_path)
 
     for i, box in enumerate(boxes):
         cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
-        bcx = (box[0] + box[2])/2
-        bcy = (box[1] + box[3])/2
+        bcx = (int(box[0]) + int(box[2]))/2
+        bcy = (int(box[1]) + int(box[3]))/2
         cv2.putText(image, f"{i}", (int(bcx), int(bcy)), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
     cv2.imwrite("output/visualized/" + image_path.split("/")[-1], image)
         
 
 def process_single_image(image_path):
-    boundaries = boundary_detector.get_boundaries(image_path)
-    left_pallet, right_pallet = pallet_detector.detect(image_path, boundaries)
-    left_boxes, right_boxes = box_detector.detect(image_path, boundaries, left_pallet, right_pallet)
+    boundaries, pallets, boxes = initialize(image_path)
+    left_pallet, right_pallet = pallet_detector.filter_and_split_pallets(pallets, boundaries, cv2.imread(image_path).shape[1])
+    left_boxes, right_boxes = box_detector.map_boxes(boxes, left_pallet, right_pallet)
     combined_boxes = left_boxes + right_boxes
     
     visualize_box_dimensions(image_path, combined_boxes)
-    # visualize_box_dimensions(image_path, right_boxes)
 
     for i, box in enumerate(combined_boxes):
-        l = round(box[2] - box[0], 2)
-        h = round(box[3] - box[1], 2)
-        print(f"\t\t{i}: {l} x {h} => {l/h:.2f}")
-
-    
+        l = round(int(box[2]) - int(box[0]), 2)
+        h = round(int(box[3]) - int(box[1]), 2)
+        print(f"\t\t{i}: {l} x {h} => {l/h:.2f} => {l*h:.2f}")
 
 if __name__ == "__main__":
     for image in sorted(os.listdir(images_dir)):
         print("\n\nProcessing:", image)
-        if image.endswith(".JPG"):
-            process_single_image(images_dir + image)
-
-        
-    
-    
+        if image.lower().endswith(".png") or image.lower().endswith(".jpg"):
+            try:
+                process_single_image(images_dir + image)
+            except Exception as e:
+                print("Error processing image:", image)
+                print(e)
