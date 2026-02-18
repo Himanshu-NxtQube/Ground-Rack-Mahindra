d5953dfebc709ebb5a311016cbb7e1a41f0ace0f
Himanshu <himanshu.nxtqube@gmail.com>
2026-02-18 18:50:23 +0530

    feat: Replaced YOLO-based pallet status estimation with rule-based logic, updated the box detection model, and streamlined the `get_box_depth` script by removing OCR and simplifying visualization.

--- src/inference/pallet_status.py
+++ src/inference/pallet_status.py
@@ -5,55 +5,76 @@ from ultralytics import YOLO
 
 class PalletStatus:
     def __init__(self):
-        self.pallet_status_estimator = YOLO("./models/new pallet status model.pt")
+        # self.pallet_status_estimator = YOLO("./models/new pallet status model.pt")
 
-    def get_status(self, image_path, depth_map):
-        image_name = os.path.basename(image_path)
-        image = cv2.imread(image_path)
-        img_dims = image.shape
-        center_h, center_w = img_dims[0]//2, img_dims[1]//2
+    # def get_status(self, image_path, depth_map):
+    #     image_name = os.path.basename(image_path)
+    #     image = cv2.imread(image_path)
+    #     img_dims = image.shape
+    #     center_h, center_w = img_dims[0]//2, img_dims[1]//2
 
-        # Apply plasma colormap
-        colored_depth = cv2.applyColorMap(depth_map, cv2.COLORMAP_PLASMA)
+    #     # Apply plasma colormap
+    #     colored_depth = cv2.applyColorMap(depth_map, cv2.COLORMAP_PLASMA)
 
-        results = self.pallet_status_estimator.predict(colored_depth, verbose=False)
-        class_names = self.pallet_status_estimator.names
+    #     results = self.pallet_status_estimator.predict(colored_depth, verbose=False)
+    #     class_names = self.pallet_status_estimator.names
 
-        left_boxes = []
-        right_boxes = []
+    #     left_boxes = []
+    #     right_boxes = []
 
-        for box in results[0].boxes:  # xyxy format: [x1, y1, x2, y2]
-            # print(box.xyxy)
-            x1, y1, x2, y2 = box.xyxy[0]
-            cv2.rectangle(colored_depth, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-            cx = (x1 + x2) // 2
-            cy = (y1 + y2) // 2
+    #     for box in results[0].boxes:  # xyxy format: [x1, y1, x2, y2]
+    #         # print(box.xyxy)
+    #         x1, y1, x2, y2 = box.xyxy[0]
+    #         cv2.rectangle(colored_depth, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+    #         cx = (x1 + x2) // 2
+    #         cy = (y1 + y2) // 2
 
-            # Filter out within ROI
+    #         # Filter out within ROI
 
-            class_id = int(box.cls[0])            
-            class_name = class_names[class_id]
+    #         class_id = int(box.cls[0])            
+    #         class_name = class_names[class_id]
 
-            if cx < center_w:
-                left_boxes.append([cx, class_name, (x1, y1, x2, y2)])
-            else:
-                right_boxes.append([cx, class_name, (x1, y1, x2, y2)])
-        cv2.imwrite(f"output/visualized/{image_name}_depth.png", colored_depth)
+    #         if cx < center_w:
+    #             left_boxes.append([cx, class_name, (x1, y1, x2, y2)])
+    #         else:
+    #             right_boxes.append([cx, class_name, (x1, y1, x2, y2)])
+    #     cv2.imwrite(f"output/visualized/{image_name}_depth.png", colored_depth)
 
-        left_box_result = None
-        right_box_result = None
+    #     left_box_result = None
+    #     right_box_result = None
 
-        left_bbox = None
-        right_bbox = None
+    #     left_bbox = None
+    #     right_bbox = None
 
-        if left_boxes:
-            left_box_result = min(left_boxes,key=lambda x:x[0])[1]
-            left_bbox = min(left_boxes,key=lambda x:x[0])[2]
-        if right_boxes:
-            right_box_result = max(right_boxes,key=lambda x:x[0])[1]
-            right_bbox = max(right_boxes,key=lambda x:x[0])[2]
+    #     if left_boxes:
+    #         left_box_result = min(left_boxes,key=lambda x:x[0])[1]
+    #         left_bbox = min(left_boxes,key=lambda x:x[0])[2]
+    #     if right_boxes:
+    #         right_box_result = max(right_boxes,key=lambda x:x[0])[1]
+    #         right_bbox = max(right_boxes,key=lambda x:x[0])[2]
 
-        # return left_box_result, right_box_result
-        return {"left_status": left_box_result, "right_status": right_box_result,
-                "left_bbox": left_bbox,           "right_bbox": right_bbox}
+    #     # return left_box_result, right_box_result
+    #     return {"left_status": left_box_result, "right_status": right_box_result,
+    #             "left_bbox": left_bbox,           "right_bbox": right_bbox}
+
+    def get_status(self, box_stacks, boxes, layers, odd_layering, even_layering):
+        if len(boxes[0]) == 0:
+            return "empty"
+
+        for i in range(1,layers):
+            if len(boxes[i]) > 0:
+                return "partial"
+            
+
+        top_stack = box_stacks[0]
+        if len(box_stacks)%2 == 0:
+            front_layer = even_layering.split('/')[0]
+        else:
+            front_layer = odd_layering.split('/')[0]
+        H, V = front_layer.split('.')
+        boxes = int(H[0]) + int(V[0])
+        if boxes == len(top_stack): 
+            return "full"
+        else:
+            return "partial"
 
